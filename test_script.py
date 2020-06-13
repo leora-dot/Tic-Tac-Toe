7c5e60b6c15115ac455a87798873daef10cc3876
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-06-12 22:37:48 -0400

    Recalculated nuber of test cases (combinations not permutations)

--- test_script.py
+++ test_script.py
@@ -10,7 +10,7 @@
     #compare results
 #CAVEATS
     #This may be impractical given the large number of possible cases -> consider random sampling
-    #Because the bulk of the possible cases come from the case in which five board spots are filled, I will ignore that case when building out testing script.
+    #Because the bulk of the possible cases come from the case in which five board spots are filled, it may make sense not to add these configurations in when I'm just building the functions.
     #That case can be added later in full or in part. But no need to wait through the processing times until the end.
 
 #importing functions needed
@@ -21,6 +21,8 @@ import matplotlib.pyplot as plt
 
 #for factorials
 import math
+#for permutation
+import itertools
 
 from game_script import \
 position_to_index, \
@@ -35,30 +37,36 @@ board_df
 
 #Functions go here
 
-#Factorial function, just a wrapper so I don't have to type.
+#Factorial function, just a wrapper so I don't have to type out the math library.
 def factorial(val):
     return math.factorial(val)
 
 #Permutation calculator
-def permutation(n,k):
-    return factorial(n)/factorial((n-k))
+
+def combination_num(n,r):
+    return factorial(n)/((factorial(r)*factorial(n-r)))
+
+def permutation_list(k, n = 9, symbol = "x"):
+    perm_list = list(itertools.permutations([1,2,3,4,5,6,7,8,9],3))
+    return perm_list
 
 #CALCULATING NUMBER OF TEST CASES
+
+#board configurations
 #At the end of the five move game a player can have filled any five spots, meaning we would need to test:
-    #all possible board configurations with five spots five spots P(9,5) + four spots P(9,4) +three spots P(9,3).
+    #all possible board configurations with five spots five spots C(9,5) + four spots C(9,4) +three spots C(9,3).
     #two spots and one spots do not need to be tested since the is_game_won function defaults to False in these conditions.
     #the number of spots we need to generate is:
 
-#print(permutation(9,5) + permutation(9,4) + permutation(9,3))
-#18,648 board configurations
+print(combination_num(9,5) + combination_num(9,4) + combination_num(9,3))
+#336 board configurations
 
-#Once we have all the combinations, we need to test each of them for all possible latest positions.
+#Once we have all the board configurations, we need to test each of them for all possible latest positions.
     #we don't need to do all nine since only the spots filled could have had a latest move.
-    #In that case we have five spots (5 x 5! = 600), four spots (4 x 4! = 96), three spots (3 x 3! = 18)
     #the number of cases to test is:
 
-#print(5*permutation(9,5) + 4*permutation(9,4) + 3*permutation(9,3))
-#89,208 test cases.
+print(5*combination_num(9,5) + 4*combination_num(9,4) + 3*combination_num(9,3))
+#1,386 test cases
 
 #POPULATING PANDAS TABLE WITH ALL TEST CASES
 
