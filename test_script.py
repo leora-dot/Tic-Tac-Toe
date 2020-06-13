ca23fabc0dd476a521071c209b149a6dfcce5854
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-06-12 22:50:13 -0400

    added combination generator function

--- test_script.py
+++ test_script.py
@@ -46,8 +46,8 @@ def factorial(val):
 def combination_num(n,r):
     return factorial(n)/((factorial(r)*factorial(n-r)))
 
-def permutation_list(k, n = 9, symbol = "x"):
-    perm_list = list(itertools.permutations([1,2,3,4,5,6,7,8,9],3))
+def combination_list(k, n = 10, symbol = "x"):
+    perm_list = list(itertools.combinations(list(range(1,n)),k))
     return perm_list
 
 #CALCULATING NUMBER OF TEST CASES
@@ -58,16 +58,18 @@ def permutation_list(k, n = 9, symbol = "x"):
     #two spots and one spots do not need to be tested since the is_game_won function defaults to False in these conditions.
     #the number of spots we need to generate is:
 
-print(combination_num(9,5) + combination_num(9,4) + combination_num(9,3))
+#print(combination_num(9,5) + combination_num(9,4) + combination_num(9,3))
 #336 board configurations
 
 #Once we have all the board configurations, we need to test each of them for all possible latest positions.
     #we don't need to do all nine since only the spots filled could have had a latest move.
     #the number of cases to test is:
 
-print(5*combination_num(9,5) + 4*combination_num(9,4) + 3*combination_num(9,3))
+#print(5*combination_num(9,5) + 4*combination_num(9,4) + 3*combination_num(9,3))
 #1,386 test cases
 
 #POPULATING PANDAS TABLE WITH ALL TEST CASES
 
 testing_df = pd.read_csv('test_is_game_won.csv')
+
+print(len((combination_list(3))))
